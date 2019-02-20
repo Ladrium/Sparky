f8bf4c4e21f61d8f8bdd3349f257b34fd8899ea0
ExTrEmE HeRo <38991749+Anish-Shobith@users.noreply.github.com>
2019-02-20 23:19:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Client } = require('klasa');
-const { config, token } = require('./config');
+const { Client } = require("klasa");
+const { config, token } = require("./config");
 
 class Sparky extends Client {
 

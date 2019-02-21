ab9be86e61abc98447cdcc46cc254fa999a45d1d
ExTrEmE HeRo <38991749+Anish-Shobith@users.noreply.github.com>
2019-02-21 23:12:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,5 @@ const { Client } = require("klasa");
 const { config, token } = require("./config");
 
 class Sparky extends Client {
-
-    constructor(...args) {
-        super(...args);
-
-        // Add any properties to your Klasa Client
-    }
-
-    // Add any methods to your Klasa Client
-
 }
-
-new Sparky(config).login(token || process.env.token);
+new Sparky(config).login(process.env.token);
